184c03cd6772d9f55682c394ea7a0b34ab7cd493
DimaBronytskyy <138112501+DimaBronytskyy@users.noreply.github.com>
2024-03-04 23:27:46 +0200

    push

---
+++ src/components/Feedback/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,11 @@
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  const buttonElements = options.map(name => (
+    <button onClick={() => onLeaveFeedback(name)} key={name}>
+      {name}
+    </button>
+  ));
+
+  return buttonElements;
+};
+
+export default FeedbackOptions;
